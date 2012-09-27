28a09614dc4a0da4efe6a84e05b1f16e982e454e
stephane.guindon <stephane.guindon@2b562f48-3654-0410-b056-8947d4ae5377>
2012-09-27 12:31:44 +0000

    Backup

--- trunk/src/mcmc.h
+++ trunk/src/mcmc.h
@@ -144,5 +144,6 @@ void MCMC_Randomize_Birth(t_tree *tree);
 void MCMC_Clock_R(t_tree *mixt_tree);
 void MCMC_Free_MCMC(t_mcmc *mcmc);
 void MCMC_Updown_T_Br(t_tree *tree);
+void MCMC_Root_Time(t_tree *tree);
 
 #endif
