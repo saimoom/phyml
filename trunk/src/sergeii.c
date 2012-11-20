ea54ed9e149b6c63d6f47ab29b2e8c5adc1b494c
krasnozhon <krasnozhon@2b562f48-3654-0410-b056-8947d4ae5377>
2012-11-20 03:34:50 +0000

    Function PhyTime_XML is updated.

--- trunk/src/sergeii.c
+++ trunk/src/sergeii.c
@@ -28,13 +28,28 @@ void PhyTime_XML(char *xml_file)
   xml_node *n_r, *n_t, *n_m, *n_cur;
   t_cal *last_calib;
   /* t_cal *cur; */
-  align **data;
+  align **data, **c_seq;
   option *io;
+  calign *cdata;
+  t_opt *s_opt;
+  int num_data_set;
+  int num_tree, num_rand_tree;
+  t_mod *mod;
+  time_t t_beg,t_end;
+  int r_seed;
+  char *most_likely_tree;
+  int user_lk_approx;
+  t_tree *tree;
+  t_node **a_nodes;
+  m4 *m4mod;
+  t_rate *rates;
+
 
   i = 0;
   j = 0;
-
-  last_calib = NULL;
+  last_calib       = NULL;
+  mod              = NULL;
+  most_likely_tree = NULL;
 
   //file can/cannot be open:
   if ((f =(FILE *)fopen(xml_file, "r")) == NULL)
@@ -45,48 +60,99 @@ void PhyTime_XML(char *xml_file)
 
   n_r = XML_Load_File(f);
 
-
+  //memory allocation for model parameters:
+  io = (option *)Make_Input();
+  mod   = (t_mod *)Make_Model_Basic();
+  s_opt = (t_opt *)Make_Optimiz();
+  m4mod = (m4 *)M4_Make_Light();
+  Set_Defaults_Input(io);                                                                          //model - GUINDON!!!!
+  Set_Defaults_Model(mod);
+  Set_Defaults_Optimiz(s_opt);  
+  io -> mod = mod;
+  mod = io -> mod;
+  
   ////////////////////////////////////////////////////////////////////////////
   //////////////////////reading tree topology:////////////////////////////////
-
-  //memory for options:
-  io = (option *)Make_Input();
 
   //looking for a node <topology>
   n_t = XML_Search_Node_Name("topology", YES, n_r);
 
   //setting tree:
-  io -> tree = Read_Tree(&n_t -> value);
+  tree        = (t_tree *)mCalloc(1,sizeof(t_tree));
+  n_cur = XML_Search_Node_Name("instance", YES, n_t);
+  if(n_cur != NULL)
+    {
+      if(XML_Search_Attribute(n_cur, "user.tree") != NULL) 
+	{
+	  strcpy(io -> out_tree_file, XML_Search_Attribute(n_cur, "user.tree") -> value); 
+	  io -> fp_out_tree  = Openfile(io -> out_tree_file, 1);
+	  io -> tree = Read_Tree_File_Phylip(io -> fp_in_tree);
+	}
+      else
+	{
+	  PhyML_Printf("\n==Tree was not found. \n");
+	  PhyML_Printf("\n==Either specify tree file name or enter the whole tree. \n");
+	  Exit("\n");
+	}
+    }
+  else io -> tree  = Read_Tree(&n_t -> value);
+  io -> n_otu = io -> tree -> n_otu;
+  tree = io -> tree;
+
+  ////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+
+  //memory for nodes:
+  a_nodes   = (t_node **)mCalloc(2 * io -> n_otu - 2,sizeof(t_node *));
+  For(i, 2 * io -> n_otu - 2) a_nodes[i] = (t_node *)mCalloc(2 * io -> n_otu - 2,sizeof(t_node));
  
-  //memory for rates:
-  io -> tree -> rates = RATES_Make_Rate_Struct(io -> tree -> n_otu);
+  //setting a model:
  
+  tree -> rates = RATES_Make_Rate_Struct(io -> n_otu);                                    
+  RATES_Init_Rate_Struct(tree -> rates, io -> rates, tree -> n_otu);
+
+  //reading seed:
+  if(XML_Search_Attribute(n_r, "seed")) io -> r_seed = String_To_Dbl(XML_Search_Attribute(n_r, "seed") -> value);
+   
   // TO DO: check that the tree has a root...
   Update_Ancestors(io -> tree -> n_root, io -> tree -> n_root -> v[0], io -> tree);
   Update_Ancestors(io -> tree -> n_root, io -> tree -> n_root -> v[1], io -> tree);
 		  
 
   ////////////////////////////////////////////////////////////////////////////
-  //////////////////////reading sequences and clades://///////////////////////
+  //////////////////////memory allocation for temp parameters/////////////////
 
   //memory for monitor flag:
-  io -> mcmc -> monitor = (int *)mCalloc(2 * io -> tree -> n_otu - 1,sizeof(int));
+  io -> mcmc -> monitor = (int *)mCalloc(2 * io -> n_otu - 1,sizeof(int));
  
   //memory for sequences:
   n_cur = XML_Search_Node_Name("alignment", YES, n_r);
 
-  data   = (align **)mCalloc(io -> tree -> n_otu,sizeof(align *));
-  For(i, io -> tree -> n_otu)
+  data   = (align **)mCalloc(io -> n_otu,sizeof(align *));
+  For(i, io -> n_otu)
     {
       data[i]          = (align *)mCalloc(1,sizeof(align));
       data[i] -> name  = (char *)mCalloc(T_MAX_NAME,sizeof(char));
-      data[i] -> state = (char *)mCalloc(strlen(n_cur -> child -> value) + 1,sizeof(char));
+      if(n_cur -> child -> value != NULL) data[i] -> state = (char *)mCalloc(strlen(n_cur -> child -> value) + 1,sizeof(char));
+      else data[i] -> state = (char *)mCalloc(T_MAX_SEQ,sizeof(char));
     }
   io -> data = data;
+  tree -> data = data;
+
+  //memory for compressed sequences:
+  cdata         = (calign *)mCalloc(1,sizeof(calign));
+  c_seq   = (align **)mCalloc(io -> n_otu,sizeof(align *));
+  For(i, io -> n_otu)
+    {
+      c_seq[i]          = (align *)mCalloc(1,sizeof(align));
+      c_seq[i] -> name  = (char *)mCalloc(T_MAX_NAME,sizeof(char));
+      c_seq[i] -> state = (char *)mCalloc(io -> state_len + 1,sizeof(char));
+    }
+
+  cdata -> c_seq = c_seq;
  
   //memory for clade:
   clade_name = (char *)mCalloc(T_MAX_NAME,sizeof(char));
-
   clade = (char **)mCalloc(T_MAX_FILE,sizeof(char *));
   For(i, T_MAX_FILE)
     {
@@ -100,6 +166,8 @@ void PhyTime_XML(char *xml_file)
       mon_list[i] = (char *)mCalloc(T_MAX_NAME,sizeof(char));
     }
 
+ ////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////
  
   //reading monitor node:
   i = 0;
@@ -122,6 +190,9 @@ void PhyTime_XML(char *xml_file)
       PhyML_Printf("\n. There is no clade to be monitored. \n");
     }
 
+ ////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////
+
   //chekcing for calibration node (upper or lower bound) to exist:
   n_cur = XML_Search_Node_Name("calibration", YES, n_r);
 
@@ -139,7 +210,10 @@ void PhyTime_XML(char *xml_file)
 	  PhyML_Printf("\n==Please check your data. \n");
 	  Exit("\n");
 	}
-    }   
+    }
+
+ ////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////   
 
   n_r = n_r -> child; 
 
@@ -147,17 +221,48 @@ void PhyTime_XML(char *xml_file)
     {
       if(!strcmp(n_r -> name, "alignment"))//looking for a node <alignment>.
 	{
-	  i = 0;
-	  do
+	  if(!n_r -> attr -> value)
+	  {
+		PhyML_Printf("\n==Not found sequence type (nt / aa). \n");
+		PhyML_Printf("\n==Please, include data to node <%s> attribute value. \n", n_r -> name);
+		Exit("\n");
+	  }	
+	  if(!strcmp(n_r -> attr -> value, "nt")) 
 	    {
-	      strcpy(io -> data[i] -> name, n_r -> child -> attr -> value);
-	      strcpy(io -> data[i] -> state, n_r -> child -> value);
-	      i++;
-	      if(n_r -> child -> next) n_r -> child = n_r -> child -> next;
-	      else break;
+	      io -> datatype = 0;
+	      io -> mod -> ns = 4;
 	    }
-	  while(n_r -> child);
-	  n_taxa = i;
+ 	  if(!strcmp(n_r -> attr -> value, "aa")) 
+	    {
+	      io -> datatype = 1;
+	      io -> mod -> ns = 20;
+	    }
+
+	  n_cur = XML_Search_Node_Name("instance", YES, n_r);
+	  if(n_cur != NULL)
+	    {
+	      if(XML_Search_Attribute(n_cur, "user.alignment") != NULL) 
+		{
+		  strcpy(io -> in_align_file, XML_Search_Attribute(n_cur, "user.alignment") -> value); 
+		  io -> fp_in_align  = Openfile(io -> in_align_file, 1);
+		  Detect_Align_File_Format(io);
+		  io -> data = Get_Seq(io);
+		}
+	    }
+	  else
+	    {	
+	      i = 0;
+	      do
+		{
+		  strcpy(io -> data[i] -> name, n_r -> child -> attr -> value);
+		  strcpy(io -> data[i] -> state, To_Upper_String(n_r -> child -> value));
+		  i++;
+		  if(n_r -> child -> next) n_r -> child = n_r -> child -> next;
+		  else break;
+		}
+	      while(n_r -> child);
+	      n_taxa = i;
+	    }
 
 	  //checking if a sequences of the same lengths:
 	  i = 1;
@@ -173,7 +278,7 @@ void PhyTime_XML(char *xml_file)
 	  For(i, n_taxa) Check_Sequence_Name(io -> data[i] -> name);
 
 	  //check if a number of tips is equal to a number of taxa:
-	  if(n_taxa != io -> tree -> n_otu)
+	  if(n_taxa != io -> n_otu)
 	    {
 	      PhyML_Printf("\n==Number of taxa is not the same as a number of tips. Check your data...\n");
 	      Exit("\n");
@@ -185,25 +290,25 @@ void PhyTime_XML(char *xml_file)
 	{
 	  low = BIG;
 	  up  = BIG;
-	  n_cur = XML_Search_Node_Name("lower", NO, n_r -> child);
-	  if(n_cur != NULL) low = String_To_Dbl( n_cur -> value);
-	  n_cur = XML_Search_Node_Name("upper", NO, n_r -> child);
+	  n_cur = XML_Search_Node_Name("lower", YES, n_r);
+	  if(n_cur != NULL) low = String_To_Dbl(n_cur -> value);
+	  n_cur = XML_Search_Node_Name("upper", YES, n_r);
 	  if(n_cur != NULL) up = String_To_Dbl(n_cur -> value);
 	  do
 	    {
 	      if(!strcmp("appliesto", n_r -> child -> name)) 
 		{
 		  strcpy(clade_name, n_r -> child -> child -> attr -> value);//reached clade names
-		  if (io -> tree -> rates -> calib == NULL) io -> tree -> rates -> calib = Make_Calib();
+		  if (io -> rates -> calib == NULL) io -> rates -> calib = Make_Calib();
 		  if(last_calib)
 		    {
-		      last_calib -> next = io -> tree -> rates -> calib;
-		      io -> tree -> rates -> calib -> prev = last_calib;
+		      last_calib -> next = io -> rates -> calib;
+		      io -> rates -> calib -> prev = last_calib;
 		    }
-		  last_calib = io -> tree -> rates -> calib;
-		  io -> tree -> rates -> calib -> lower = low;
+		  last_calib = io -> rates -> calib;
+		  io -> rates -> calib -> lower = low;
 		  //PhyML_Printf("\n. '%f'\n", io -> tree -> rates -> calib -> lower);
-		  io -> tree -> rates -> calib -> upper = up;
+		  io -> rates -> calib -> upper = up;
 		  //PhyML_Printf("\n. '%f'\n", io -> tree -> rates -> calib -> upper);
 		  if(!strcmp("@root@", clade_name))
 		    {
@@ -231,14 +336,13 @@ void PhyTime_XML(char *xml_file)
 			}	
 		    }
 
-		      io -> tree -> rates -> calib -> node_num = node_num;
+		      io -> rates -> calib -> node_num = node_num;
 		      //PhyML_Printf("\n. '%d'\n", io -> tree -> rates -> calib -> node_num);
-		      io -> tree -> rates -> t_prior_min[node_num] = low;
-		      io -> tree -> rates -> t_prior_max[node_num] = up;
-		      io -> tree -> rates -> t_has_prior[node_num] = YES;
-		      io -> tree -> rates -> calib -> proba = String_To_Dbl(n_r -> child -> attr -> value);
+		      tree -> rates -> t_prior_min[node_num] = low;
+		      tree -> rates -> t_prior_max[node_num] = up;
+		      tree -> rates -> t_has_prior[node_num] = YES;
 		      //PhyML_Printf("\n. '%f'\n", io -> tree -> rates -> calib -> proba);
-		      io -> tree -> rates -> calib = io -> tree -> rates -> calib -> next;
+		      io -> rates -> calib = io -> rates -> calib -> next;
  		      if(n_r -> child -> next) n_r -> child = n_r -> child -> next;
 		      else break;
 		}
@@ -254,19 +358,13 @@ void PhyTime_XML(char *xml_file)
     }
   while(n_r);
 
-  io -> tree -> rates -> calib = last_calib;
-  while(io -> tree -> rates -> calib -> prev) io -> tree -> rates -> calib = io -> tree -> rates -> calib -> prev;
-  /* cur = io -> tree -> rates -> calib; */
-  /*
-  do
-    {
-      PhyML_Printf("\n '%d' \n", cur -> node_num);
-      if(cur -> next) cur = cur -> next;
-      else break;
-    }
-  while(cur);  
-  */
+  io -> rates -> calib = last_calib;
+  while(io -> rates -> calib -> prev) io -> rates -> calib = io -> rates -> calib -> prev;
 
+ ////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////   
+
+  //clear memory:
   free(clade_name);
   For(i, T_MAX_FILE)
     {
@@ -280,12 +378,144 @@ void PhyTime_XML(char *xml_file)
     }
   free(mon_list);
 
-  //PhyML_Printf("\n. '%f' '%f'\n", io -> tree -> rates -> t_prior_min[11], io -> tree -> rates -> t_prior_max[11]);
-  //PhyML_Printf("\n. '%f' '%f'\n", io -> tree -> rates -> t_prior_min[18], io -> tree -> rates -> t_prior_max[18]);
+ ////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////   
 
+  //START analysis:
+  r_seed = (io -> r_seed < 0)?(time(NULL)):(io -> r_seed);
+  srand(r_seed); 
+  rand(); 	
+  PhyML_Printf("\n. Seed: %d\n", r_seed);
+  PhyML_Printf("\n. Pid: %d\n",getpid()); 
+  PhyML_Printf("\n. Compressing sequences...\n");
+  data = io -> data;
+  data[0] -> len = strlen(data[0] -> state);
+  cdata = Compact_Data(data, io);
+  Free_Seq(io -> data, cdata -> n_otu);
+  io -> mod -> io = io;
+  Check_Ambiguities(cdata, io -> mod -> io -> datatype, io -> state_len);            
+  mod -> s_opt = s_opt;
+  Make_Model_Complete(mod);                           
+  Init_Model(cdata, mod, io);
+  if(io -> mod -> use_m4mod) M4_Init_Model(mod -> m4mod, cdata, mod);
+  time(&(t_beg));
 
+  tree -> mod         = mod;                                                                    
+  tree -> io          = io;
+  tree -> data        = cdata;
+  tree -> n_pattern   = tree -> data -> crunch_len / tree -> io -> state_len; 
+
+  Set_Both_Sides(YES, tree);
+  Prepare_Tree_For_Lk(tree);
+  TIMES_Set_All_Node_Priors(tree);
+
+  TIMES_Get_Number_Of_Time_Slices(tree);
+  TIMES_Label_Edges_With_Calibration_Intervals(tree);
+
+  tree -> write_br_lens = NO;	
+
+  PhyML_Printf("\n");														
+  PhyML_Printf("\n. Input tree with calibration information ('almost' compatible with MCMCtree).\n");
+  PhyML_Printf("\n. %s \n", Write_Tree(tree, YES));
+
+  tree -> write_br_lens = YES;
+
+  // Work with log of branch lengths?
+  if(tree -> mod -> log_l == YES) Log_Br_Len(tree);
+
+  if(io -> mcmc -> use_data == YES)																
+    {
+      // Force the exact likelihood score 
+      user_lk_approx = tree -> io -> lk_approx;													
+      tree -> io -> lk_approx = EXACT;
+		      
+      // MLE for branch lengths 																
+      PhyML_Printf("\n");
+      Round_Optimize(tree, tree -> data, ROUND_MAX);
+		      
+      // Set vector of mean branch lengths for the Normal approximation of the likelihood 
+      RATES_Set_Mean_L(tree);
+		      
+      // Estimate the matrix of covariance for the Normal approximation of the likelihood
+      PhyML_Printf("\n");
+      PhyML_Printf("\n. Computing Hessian...");												    
+      tree -> rates -> bl_from_rt = 0;																		
+      Free(tree -> rates -> cov_l);																			
+      tree -> rates -> cov_l = Hessian_Seo(tree);
+															 
+      // tree->rates->cov_l = Hessian_Log(tree); 
+      For(i, (2 * tree -> n_otu - 3) * (2 * tree -> n_otu - 3)) tree -> rates -> cov_l[i] *= -1.0;
+      if(!Iter_Matinv(tree -> rates -> cov_l, 2 * tree -> n_otu - 3, 2 * tree -> n_otu - 3, YES)) Exit("\n");			
+      tree -> rates -> covdet = Matrix_Det(tree -> rates -> cov_l, 2 * tree -> n_otu - 3, YES);							
+      For(i,(2 * tree -> n_otu - 3) * (2 * tree -> n_otu - 3)) tree -> rates -> invcov[i] = tree -> rates -> cov_l[i];  
+      if(!Iter_Matinv(tree -> rates -> invcov, 2 * tree -> n_otu - 3, 2 * tree -> n_otu - 3, YES)) Exit("\n");
+      tree -> rates -> grad_l = Gradient(tree);					
+
+      // Pre-calculation of conditional variances to speed up calculations 					
+      RATES_Bl_To_Ml(tree);
+      RATES_Get_Conditional_Variances(tree);
+      RATES_Get_All_Reg_Coeff(tree);
+      RATES_Get_Trip_Conditional_Variances(tree);
+      RATES_Get_All_Trip_Reg_Coeff(tree);
+		      
+      Lk(NULL, tree);
+      PhyML_Printf("\n");
+      PhyML_Printf("\n. p(data|model) [exact ] ~ %.2f",tree -> c_lnL);																			
+      tree -> io -> lk_approx = NORMAL;															
+      For(i,2 * tree -> n_otu - 3) tree -> rates -> u_cur_l[i] = tree -> rates -> mean_l[i] ;				
+      tree -> c_lnL = Lk(NULL,tree);
+      PhyML_Printf("\n. p(data|model) [approx] ~ %.2f",tree -> c_lnL);
+
+      tree -> io -> lk_approx = user_lk_approx;	
+										
+    }
+
+  tree -> rates -> model = io -> rates -> model;
+  													  
+  PhyML_Printf("\n. Selected model '%s' \n", RATES_Get_Model_Name(io -> rates -> model));
+
+  if(tree -> rates -> model == GUINDON) tree -> mod -> gamma_mgf_bl = YES;														
+  tree -> rates -> bl_from_rt = YES;
+
+  if(tree -> io -> cstr_tree) Find_Surviving_Edges_In_Small_Tree(tree, tree -> io -> cstr_tree); 																				 
+  time(&t_beg);
+
+  tree -> mcmc = MCMC_Make_MCMC_Struct();
+ 
+  MCMC_Copy_MCMC_Struct(tree -> io -> mcmc, tree -> mcmc, "phytime"); 
+
+  tree -> mod -> m4mod = m4mod;
+  	
+  MCMC_Complete_MCMC(tree -> mcmc, tree);
+
+  tree -> mcmc -> is_burnin = NO;
+ 
+
+  MCMC(tree);                                            																
+  MCMC_Close_MCMC(tree -> mcmc);																	
+  MCMC_Free_MCMC(tree -> mcmc);														 				
+  PhyML_Printf("\n");	
+ 
+  Free_Tree_Pars(tree);
+  Free_Tree_Lk(tree);
+  Free_Tree(tree);
+  Free_Cseq(cdata);
+  Free_Model(mod);
+  if(io -> fp_in_align)   fclose(io -> fp_in_align);
+  if(io -> fp_in_tree)    fclose(io -> fp_in_tree);
+  if(io -> fp_out_lk)     fclose(io -> fp_out_lk);
+  if(io -> fp_out_tree)   fclose(io -> fp_out_tree);
+  if(io -> fp_out_trees)  fclose(io -> fp_out_trees);
+  if(io -> fp_out_stats)  fclose(io -> fp_out_stats);
   fclose(f);
+  Free(most_likely_tree);
+  Free_Input(io);
 
+  time(&t_end);
+  Print_Time_Info(t_beg,t_end);	
+ 	
   /* return 1;    */
 }
+
+
 
