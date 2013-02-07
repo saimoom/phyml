ece3ddcc317fa2e7e53c4fa29bf8629b1d708cdb
stephane.guindon <stephane.guindon@2b562f48-3654-0410-b056-8947d4ae5377>
2013-02-07 20:20:09 +0000

    Transfer to nesi

--- trunk/src/init.c
+++ trunk/src/init.c
@@ -603,7 +603,7 @@ void Set_Defaults_Model(t_mod *mod)
 
 #if !(defined PHYTIME || defined SERGEII)
   mod->l_min = 1.E-8;
-  mod->l_max = 10.0;
+  mod->l_max = 20.0;
 #else
   mod->l_min = 1.E-8;
   mod->l_max = 2.0;
