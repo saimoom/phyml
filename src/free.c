d1cbf691188940c4d01de61ed57383786d8e712c
stephane.guindon <stephane.guindon@2b562f48-3654-0410-b056-8947d4ae5377>
2013-10-29 08:43:44 +0000

    Fixed memory bug in PhyTime

--- src/free.c
+++ src/free.c
@@ -338,9 +338,9 @@ void Free_Tree_Pars(t_tree *mixt_tree)
 
 void Free_Edge_Pars_Left(t_edge *b)
 {
-  if(b->pars_l)   Free(b->pars_l);
-  if(b->ui_l)     Free(b->ui_l);
-  if(b->p_pars_l) Free(b->p_pars_l);
+  if(b->pars_l)          Free(b->pars_l);
+  if(b->ui_l)            Free(b->ui_l);
+  if(b->p_pars_l)        Free(b->p_pars_l);
   if(b->n_diff_states_l) Free(b->n_diff_states_l);
 }
 
